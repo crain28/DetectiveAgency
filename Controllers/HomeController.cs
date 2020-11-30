46b04eb45ab37c5ebdb799d9d7222c4e7fbce317
Cole Crain <cwcrain2000@outlook.com>
2020-11-30 13:16:53 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DetectiveAgency.Models;
+
+namespace DetectiveAgency.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CalcPay()
+        {
+            ViewBag.Salary = 0;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CalcPay(Employee model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Salary = model.CalcSalary();
+            }
+            else
+            {
+                ViewBag.Salary = 0;
+            }
+            return View(model);
+        }
+        public IActionResult mainOfficeEmployees()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DetectiveAgency.Models
+{
+    public class Employee
+    {
+        [Required]
+        public int Id { get; set; }
+        [StringLength(30)]
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [Range(10, 75, ErrorMessage = "Hours must be between 10 and 75.")]
+        public double Hours { get; set; }
+       [Required]
+       [Range(15,75, ErrorMessage ="Rate should be between $15 and $75.")]
+       public double Rate { get; set; }
+      public double CalcSalary()
+        {
+            double grossPay = 0;
+            double overtime = 0;
+            double regPay = 0;
+            if (Hours>40)
+            {
+                regPay = 40 * Rate;
+                overtime = (Hours - 40) * (1.5 * Rate);
+            }
+            else
+            {
+                regPay = Hours * Rate;
+            }
+            grossPay = regPay + overtime;
+            return grossPay;
+        }
+       
+    }
+}
